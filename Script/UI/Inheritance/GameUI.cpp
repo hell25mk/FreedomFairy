a1c7d5ecdb7786f4ddfc7a73a31c1e8eea72d68b
hell25mk <hell25mk@gmail.com>
2018-10-31 10:13:21 +0900

    [update] UIの色変更
    Scoreの表示を赤から緑に変更

--- Script/UI/Inheritance/GameUI.cpp
+++ Script/UI/Inheritance/GameUI.cpp
@@ -4,6 +4,7 @@
 #include "../../System/Score/Manager/ScoreManager.h"
 
 const int Color_Red = GetColor(255, 0, 0);
+const int Color_Green = GetColor(0, 255, 0);
 const int Color_Blue = GetColor(0, 0, 255);
 const int Font_Size = 22;
 
@@ -34,8 +35,8 @@ void GameUI::Draw(){
 	DrawBox(vec2.GetDx(), vec2.GetDy(), vec2.GetDx() + width, vec2.GetDy() + height, Color_Blue, true);
 
 	SetFontSize(Font_Size);
-	DrawFormatString(vec2.GetDx() + 10, vec2.GetDy() + 10, Color_Red, "HiScore %09d", ScoreManager::GetInstance().GetScore(eScore_High));
-	DrawFormatString(vec2.GetDx() + 10, vec2.GetDy() + 40, Color_Red, "  Score %09d", ScoreManager::GetInstance().GetScore(eScore_Now));
+	DrawFormatString(vec2.GetDx() + 10, vec2.GetDy() + 10, Color_Green, "HiScore %09d", ScoreManager::GetInstance().GetScore(eScore_High));
+	DrawFormatString(vec2.GetDx() + 10, vec2.GetDy() + 40, Color_Green, "  Score %09d", ScoreManager::GetInstance().GetScore(eScore_Now));
 	SetFontSize(DEFAULT_FONT_SIZE);
 
 }
