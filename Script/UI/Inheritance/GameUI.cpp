43e48122b6dabcb96b9246dd9df6c762c418ff5e
hell25mk <hell25mk@gmail.com>
2018-10-31 14:22:27 +0900

    [update] enumの命名変更
    enumの定義名を先頭eをつけるように変更
    値の命名を簡略化

--- Script/UI/Inheritance/GameUI.cpp
+++ Script/UI/Inheritance/GameUI.cpp
@@ -8,9 +8,9 @@ const int Color_Green = GetColor(0, 255, 0);
 const int Color_Blue = GetColor(0, 0, 255);
 const int Font_Size = 22;
 
-enum ScoreType{
-	eScore_High,
-	eScore_Now,
+enum eScoreType{
+	High,
+	Now,
 };
 
 GameUI::GameUI(){
@@ -35,8 +35,8 @@ void GameUI::Draw(){
 	DrawBox(vec2.GetDx(), vec2.GetDy(), vec2.GetDx() + width, vec2.GetDy() + height, Color_Blue, true);
 
 	SetFontSize(Font_Size);
-	DrawFormatString(vec2.GetDx() + 10, vec2.GetDy() + 10, Color_Green, "HiScore %09d", ScoreManager::GetInstance().GetScore(eScore_High));
-	DrawFormatString(vec2.GetDx() + 10, vec2.GetDy() + 40, Color_Green, "  Score %09d", ScoreManager::GetInstance().GetScore(eScore_Now));
+	DrawFormatString(vec2.GetDx() + 10, vec2.GetDy() + 10, Color_Green, "HiScore %09d", ScoreManager::GetInstance().GetScore(eScoreType::High));
+	DrawFormatString(vec2.GetDx() + 10, vec2.GetDy() + 40, Color_Green, "  Score %09d", ScoreManager::GetInstance().GetScore(eScoreType::Now));
 	SetFontSize(DEFAULT_FONT_SIZE);
 
 }
