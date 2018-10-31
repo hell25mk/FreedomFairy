d9f79d553de7e1e72d14e83091965de2ffedd73e
hell25mk <hell25mk@gmail.com>
2018-10-31 13:22:43 +0900

    [add] 様々な場所で使う定数クラスを追加
    Windowの縦横幅や円周率を持つクラス

---
+++ Script/Define/Define.cpp
@@ -0,0 +1,6 @@
+#include "Define.h"
+
+const int Define::Window_Width = 640;
+const int Define::Window_Height = 480;
+
+const float Define::Df_PAI = 3.141592654f;
---
+++ Script/Define/Define.h
@@ -0,0 +1,12 @@
+#pragma once
+
+class Define final{
+
+public:
+	const static int Window_Width;
+	const static int Window_Height;
+
+	const static float Df_PAI;
+
+};
+
