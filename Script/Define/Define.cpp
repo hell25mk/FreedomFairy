65e386f24cb0a04609e2a57a7759d24350bd56cf
hell25mk <hell25mk@gmail.com>
2018-11-02 08:29:35 +0900

    [update] Defineファイルの命名変更
    DefineからWindowに変更

--- Script/Define/Define.cpp
+++
@@ -1,6 +0,0 @@
-#include "Define.h"
-
-const int Define::Window_Width = 640;
-const int Define::Window_Height = 480;
-
-const float Define::Defi_Pai = 3.141592654f;
---
+++ Script/Define/Window.cpp
@@ -0,0 +1,6 @@
+#include "Define.h"
+
+const int Window::Window_Width = 640;
+const int Window::Window_Height = 480;
+
+const float Window::Defi_Pai = 3.141592654f;
