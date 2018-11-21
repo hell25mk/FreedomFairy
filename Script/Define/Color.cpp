e696ca4687c8bcd517de63bfe3ebcfd0e3418e7e
hell25mk <hell25mk@gmail.com>
2018-11-21 09:47:08 +0900

    [add] 色の追加
    SkyBlueを追加

--- Script/Define/Color.cpp
+++ Script/Define/Color.cpp
@@ -8,4 +8,5 @@ const int Color::Color_Red = GetColor(255, 0, 0);
 const int Color::Color_Green = GetColor(0, 255, 0);
 const int Color::Color_Blue = GetColor(0, 0, 255);
 const int Color::Color_FBlue = GetColor(0, 62, 128);
+const int Color::Color_SkyBlue = GetColor(0, 210, 255);
 const int Color::Color_Black = GetColor(0, 0, 0);
