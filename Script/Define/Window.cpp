db4360af179b132a7b35e43ab10c7fa8ac879b99
hell25mk <hell25mk@gmail.com>
2018-11-21 09:33:23 +0900

    [update] windowサイズの調整
    windowサイズを簡易的に変更できるように変更

--- Script/Define/Window.cpp
+++ Script/Define/Window.cpp
@@ -7,9 +7,10 @@ const int Win::Window_Width	 = 640;
 const int Win::Window_Height = 480;
 
 //ゲーム画面サイズ
-const int Win::Out_Width	 = (int)(Window_Width * 0.7);
+const int Win::Out_Width	 = (int)(Window_Width * 0.75);
 const int Win::Out_Height	 =		 Window_Height;
-const int Win::In_Width		 = (int)(Window_Width * 0.6);
+//const int Win::In_Width		 = (int)(Window_Width * 0.6);
+const int Win::In_Width		 = (int)(Out_Width * 0.858);
 const int Win::In_Height	 = (int)(Window_Height * 0.95);
 const int Win::Center_Px	 = (int)(Out_Width / 2);
 const int Win::Center_Py	 = (int)(Out_Height / 2);
